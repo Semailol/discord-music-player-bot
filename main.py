017bf1567cfe6754fb5e6d5c250e399d2ea56bea
Semailol <143439667+Semailol@users.noreply.github.com>
2024-01-13 20:12:30 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import nextcord
+from nextcord.ext import commands
+from pytube import YouTube
+import os
+import datetime
+bot = commands.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.slash_command(name='play', description='Play a song from YouTube')
+async def play(interaction: nextcord.Interaction, url: str):
+    user = interaction.user
+    channel = user.voice.channel
+    voice = await channel.connect()
+
+    yt = YouTube(url)
+    stream = yt.streams.filter(only_audio=True).first()
+    stream.download(filename="song.mp3")
+
+    def after():
+        os.remove("song.mp3")
+    voice.play(nextcord.FFmpegPCMAudio("song.mp3"), after=after)
+
+    title = yt.title
+    views = yt.views
+    thumbnail = yt.thumbnail_url
+    pd = yt.publish_date.strftime("%B %d, %Y")
+    
+    duration = str(datetime.timedelta(seconds=yt.length)).lstrip('0:')
+    embed = nextcord.Embed(
+        title=f"Joined",
+        color=nextcord.Color.blue()
+    )
+    embed.add_field(name="Views", value=f"{views:,}")
+    embed.add_field(name="Duration", value=duration)
+    embed.add_field(name="Published in", value=pd)
+    embed.add_field(name="Video Title", value=title)
+    embed.set_image(url=thumbnail)
+
+    await interaction.response.send_message(embed=embed)
+
+bot.run('bot-token')
